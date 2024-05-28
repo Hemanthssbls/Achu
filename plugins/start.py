b0f931c16f2b29a583c0de5a8da6b7604d74cc38
HEMANTH S S <hemanthssbls@gmail.com>
2024-05-28 20:28:41 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -123,7 +123,7 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(
-                "Join Channel",
+                "ꕤ Jᴏɪɴ Cʜᴀɴɴᴇʟ ꕤ",
                 url = client.invitelink)
         ]
     ]
@@ -131,7 +131,7 @@ async def not_joined(client: Client, message: Message):
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text = 'Try Again',
+                    text = 'ꕤ Tʀʏ Aɢᴀɪɴ ꕤ',
                     url = f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]
