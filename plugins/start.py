f77ec3ee09c6f0cd89432405ffb7796f0a3ba48e
HEMANTH S S <hemanthssbls@gmail.com>
2024-05-28 20:30:57 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("❀ Aʙᴏᴜᴛ Mᴇ ❀", callback_data = "about"),
+                    InlineKeyboardButton("❀ Cʟᴏsᴇ ❀", callback_data = "close")
                 ]
             ]
         )
